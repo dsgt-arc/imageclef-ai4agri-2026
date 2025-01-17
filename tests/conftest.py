ec7dc097cc30a24d5c5494c5d44d6073fdf676ca
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2025-01-16 20:51:17 -0800

    Initial commit with rough directory structure

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+from my_task_package.spark import get_spark
+
+@pytest.fixture(scope="session")
+def spark(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp("spark_data")
+    with spark_resource(local_dir=tmp_path.as_posix()) as spark:
+        yield spark
