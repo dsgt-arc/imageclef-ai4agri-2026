1609eeb8d7f1a00bfb21f6f495933a24fc570e0e
Hwaai Kang Kee <hkee7@gatech.edu>
2026-03-13 18:29:23 +0800

    002-utae: slurm script

--- scripts/hkee7-002-utae.sh
+++ scripts/hkee7-002-utae.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=002-utae
 #SBATCH --account=paceship-clef2026_img_ai4agri
 #SBATCH --gres=gpu:1
-#SBATCH --constraint=RTX6000
+#SBATCH --constraint=gpu-rtxpro-blackwell
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=64G
 #SBATCH --time=16:00:00
@@ -20,4 +20,4 @@ cd ~/ps-clef2026_img_ai4agri-0/imageclef-ai4agri-2026 || exit
 uv sync --package 002-utae
 
 # Run experiment
-uv run user/hkee7/experiments/002-utae/train.py --batch-size 8 --lr 0.001 --num-workers 4
+uv run user/hkee7/experiments/002-utae/train.py --mode ordinal --batch-size 32 --lr 0.001 --num-workers 4 --epochs 100
